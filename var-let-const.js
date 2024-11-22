63a35ad8a73c1fc24bee99e7b01e2478c96249d5
brunoressiguierbarbosa <bruno.ressiguier.barbosa@escola.pr.gov.br>
2024-11-22 09:07:49 -0300

    carga_js-var-let-const

---
+++ var-let-const.js
@@ -0,0 +1,30 @@
+//var
+
+//var altura = 5;
+//var comprimento = 7;
+
+//area = altura*comprimento;
+//console.log(area)
+//var area;
+
+//let forma = 'reatangulo'
+//let altura = 5;
+//let comprimento = 7;
+//let area;
+
+//if(forma ==='retangulo'){
+    //area = altura*comprimento;
+    //}else{
+    //area = (area*comprimento)/2;
+//}
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero/numeroPontoFlutuante;
+
+console.log(novaOperacao)
+//NaN -> Not A Number(nao e um numero)
+
+const alura = "Altura";
+console.log(altura*primeiroNumero)
